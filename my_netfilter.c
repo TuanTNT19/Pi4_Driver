a1b6b986fdc258d5e7cfd9d8a581a26db84300a0
TuanTNT19 <tuantnt08@gmail.com>
2026-02-08 20:34:19 +0700

    test

--- my_netfilter.c
+++ my_netfilter.c
@@ -12,7 +12,7 @@
 static unsigned int my_hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
 static struct nf_hook_ops my_hook = {
     .hook = (nf_hookfn *)my_hook_func,
-    .hooknum = NF_INET_LOCAL_IN,
+    .hooknum = NF_INET_POST_ROUTING,
     .pf = NFPROTO_IPV4,
     .priority = NF_IP_PRI_FIRST,
 };
@@ -55,26 +55,24 @@ static unsigned int my_hook_func(void *priv, struct sk_buff *skb, const struct n
             hook_name = "UNKNOWN";
             break;
     }
+    pr_info ("This is %s chain \n", hook_name);
 
-    struct iphdr *iph = ip_hdr(skb);
-    __u8 L3_proto = iph->protocol;
-    __be32 src_ip = iph->saddr;
-    __be32 des_ip = iph->daddr;
-    char src_ip_str[16], des_ip_str[16];
-    snprintf(src_ip_str, sizeof(src_ip_str), "%pI4", &src_ip);
-    snprintf(des_ip_str, sizeof(des_ip_str), "%pI4", &des_ip);
-    if (L3_proto == IPPROTO_ICMP) {
-        pr_info ("This is ICMP \n");
-        if (!strcmp (des_ip_str, "192.168.1.101")) {
-            pr_info ("Packet from %s to %s  --> DROP \n", src_ip_str, des_ip_str);
-            return NF_DROP;
-        }
-        else {
-            pr_info ("Packet from %s to %s  --> ACCEPT \n", src_ip_str, des_ip_str);
-            return NF_ACCEPT;
+    struct ethhdr *eth = eth_hdr(skb);
+    if (eth->h_proto == ntohs(ETH_P_IP)) {
+        struct iphdr *iph = ip_hdr(skb);
+        __u8 L3_proto = iph->protocol;
+        __be32 src_ip = iph->saddr;
+        __be32 des_ip = iph->daddr;
+        char src_ip_str[16], des_ip_str[16];
+        snprintf(src_ip_str, sizeof(src_ip_str), "%pI4", &src_ip);
+        snprintf(des_ip_str, sizeof(des_ip_str), "%pI4", &des_ip);
+        if (L3_proto == IPPROTO_UDP) {
+            pr_info ("This is UDP package\n");
+            pr_info ("This init source IP is : %s\n", src_ip_str);
+            pr_info ("Change IP source to 192.168.1.200 ...\n");
+            iph->saddr = in_aton("192.168.1.200");
         }
     }
-
     return NF_ACCEPT;
 }
 
